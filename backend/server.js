38f2c412a056ce634aea75e94579ff05118a7006
Kuugang <77537302+Kuugang@users.noreply.github.com>
2023-10-19 18:34:50 +0800

    .

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000
 const app = express();
 app.use(cors())
-
+console.log("asd");
 connectDB()
 
 app.use('/api/apps', require('./routes/appRoutes'))
