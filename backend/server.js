64a3c3b4f6d4b46f1c07fcd7a5b04464f46ba964
Kuugang <77537302+Kuugang@users.noreply.github.com>
2023-10-19 18:20:01 +0800

    Migrate to Vercel

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,13 @@ if (process.env.NODE_ENV === 'production') {
 } else {
     // app.get('/', (req, res) => res.send('Please set to production'))
 
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    // app.use(express.static(path.join(__dirname, '../frontend/build')))
   
-    app.get('*', (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-      )
-    )
+    // app.get('*', (req, res) =>
+    //   res.sendFile(
+    //     path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    //   )
+    // )
 }
 
 const server = app.listen(port, function() {
